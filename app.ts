de3164d512e8201dea398564c271e6cbdb70f8d9
Mathew <mateusz.lu123@gmail.com>
2023-06-24 00:18:44 +0200

    first commit

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import {google} from "googleapis"
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000 
+
+app.listen(PORT, () => {
+  console.log('Server is running on port 3000');
+});
+
+export default app
