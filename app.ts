ea07bb81e8dfd82b4f40ff72e9675b9d272803d1
Mathew <mateusz.lu123@gmail.com>
2023-06-24 16:17:50 +0200

    fix

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
-import {google} from "googleapis"
+//import {google} from "googleapis"
+import v8 from 'v8'
 
 const app = express();
 const used = process.memoryUsage();
@@ -13,10 +14,13 @@ app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
 
+const maxHeapSize = v8.getHeapStatistics().heap_size_limit;
+console.log(`Maximum heap size: ${Math.round(maxHeapSize / 1024 / 1024)} MB`);
 console.log(`Memory usage:`);
 console.log(`- Heap total: ${Math.round(used.heapTotal / 1024 / 1024)} MB`);
 console.log(`- Heap used: ${Math.round(used.heapUsed / 1024 / 1024)} MB`);
 console.log(`- Resident set size (RSS): ${Math.round(used.rss / 1024 / 1024)} MB`);
 console.log(`- External: ${Math.round(used.external / 1024 / 1024)} MB`);
+
 
 export default app
