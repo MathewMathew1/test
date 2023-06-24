a0c97ea40afb2ae35da0b2d7cb7c0902a75112a5
Mathew <mateusz.lu123@gmail.com>
2023-06-24 16:00:24 +0200

    fix

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
-import google from "googleapis"
+//import {google} from "googleapis"
 
 const app = express();
-
+const used = process.memoryUsage();
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
@@ -12,5 +12,11 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
+
+console.log(`Memory usage:`);
+console.log(`- Heap total: ${Math.round(used.heapTotal / 1024 / 1024)} MB`);
+console.log(`- Heap used: ${Math.round(used.heapUsed / 1024 / 1024)} MB`);
+console.log(`- Resident set size (RSS): ${Math.round(used.rss / 1024 / 1024)} MB`);
+console.log(`- External: ${Math.round(used.external / 1024 / 1024)} MB`);
 
 export default app
